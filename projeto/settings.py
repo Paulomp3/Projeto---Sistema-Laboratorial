ef3e62439fd67817418b3424701ff9bd1369a910
Paulo silva de Carvalho Vasconcelos <paulosvasconcelos99@outlook.com>
2026-03-13 00:57:36 -0300

    feat: configuração do banco de dados

--- projeto/settings.py
+++ projeto/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nome_do_seu_banco',
+        'USER': 'seu_usuario',
+        'PASSWORD': 'sua_senha',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
