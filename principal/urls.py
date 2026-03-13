31fcf6cfe587397895e6091fde93adb2659d8092
Paulo silva de Carvalho Vasconcelos <paulosvasconcelos99@outlook.com>
2026-03-13 00:23:28 -0300

    feat: criação das paginas html e css do sistema laboratorial, assimo como as urls e views das mesmas

--- principal/urls.py
+++ principal/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
 from principal import views
 
+
 urlpatterns = [
     path('', views.login, name='login'),
+    path('busca_metodologias/', views.busca_metodologias, name='busca_metodologias'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('emprestimo_externo/', views.emprestimo_externo, name='emprestimo_externo'),
+    path('emprestimo_interno/', views.emprestimo_interno, name='emprestimo_interno'),
+    path('emprestimos/', views.emprestimos, name='emprestimos'),
+    path('inventario_equipamentos/', views.inventario_equipamentos, name='inventario_equipamentos'),
+    path('laboratorios/', views.laboratorios, name='laboratorios'),
+    path('novo_agendamento/', views.novo_agendamento, name='novo_agendamento'),
 ]
+
+
+
--- principal/views.py
+++ principal/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return render(request, 'principal/pages/login.html')
+    return render(request, 'pages/login.html')
+def busca_metodologias(request):
+    return render(request, 'pages/busca_metodologias.html')
+def cadastro(request):
+    return render(request, 'pages/cadastro.html')
+def dashboard(request):
+    return render(request, 'pages/dashboard.html')
+def emprestimo_externo(request):
+    return render(request, 'pages/emprestimo_externo.html')
+def emprestimo_interno(request):
+    return render(request, 'pages/emprestimo_interno.html')
+def emprestimos(request):
+    return render(request, 'pages/emprestimos.html')
+def inventario_equipamentos(request):
+    return render(request, 'pages/inventario_equipamentos.html')
+def laboratorios(request):
+    return render(request, 'pages/laboratorios.html')
+def novo_agendamento(request):
+    return render(request, 'pages/novo_agendamento.html')
