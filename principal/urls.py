9517d344fb8c17918b7df1433cc1cb364ce6f86e
Paulo silva de Carvalho Vasconcelos <paulosvasconcelos99@outlook.com>
2026-03-13 23:38:06 -0300

    fix: Criação de pastas proprias para o tipo de template delas de acordo com o banco.

--- principal/urls.py
+++ principal/urls.py
@@ -4,15 +4,17 @@ from principal import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('busca_metodologias/', views.busca_metodologias, name='busca_metodologias'),
-    path('cadastro/', views.cadastro, name='cadastro'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('laboratorios/', views.laboratorios, name='laboratorios'),
+    path('agendamentos/', views.agendamentos, name='agendamentos'),
+    path('novo_agendamento/', views.novo_agendamento, name='novo_agendamento'),
+    path('emprestimos/', views.emprestimos, name='emprestimos'),
     path('emprestimo_externo/', views.emprestimo_externo, name='emprestimo_externo'),
     path('emprestimo_interno/', views.emprestimo_interno, name='emprestimo_interno'),
-    path('emprestimos/', views.emprestimos, name='emprestimos'),
+    path('busca_metodologias/', views.busca_metodologias, name='busca_metodologias'),
+    path('cadastro/', views.cadastro, name='cadastro'),
     path('inventario_equipamentos/', views.inventario_equipamentos, name='inventario_equipamentos'),
-    path('laboratorios/', views.laboratorios, name='laboratorios'),
-    path('novo_agendamento/', views.novo_agendamento, name='novo_agendamento'),
+    
 ]
 
 
--- principal/views.py
+++ principal/views.py
@@ -2,22 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return render(request, 'pages/login.html')
+    return render(request, 'pages/Login/login.html')
+
 def busca_metodologias(request):
-    return render(request, 'pages/busca_metodologias.html')
+    return render(request, 'pages/Metodologias/busca_metodologias.html')
+
 def cadastro(request):
-    return render(request, 'pages/cadastro.html')
+    return render(request, 'pages/Inventario/cadastro.html')
+
 def dashboard(request):
-    return render(request, 'pages/dashboard.html')
+    return render(request, 'pages/Dashboard/dashboard.html')
+
 def emprestimo_externo(request):
-    return render(request, 'pages/emprestimo_externo.html')
+    return render(request, 'pages/Emprestimos/emprestimo_externo.html')
+
 def emprestimo_interno(request):
-    return render(request, 'pages/emprestimo_interno.html')
+    return render(request, 'pages/Emprestimos/emprestimo_interno.html')
+
 def emprestimos(request):
-    return render(request, 'pages/emprestimos.html')
+    return render(request, 'pages/Emprestimos/emprestimos.html')
+
 def inventario_equipamentos(request):
-    return render(request, 'pages/inventario_equipamentos.html')
+    return render(request, 'pages/Inventario/inventario_equipamentos.html')
+
 def laboratorios(request):
-    return render(request, 'pages/laboratorios.html')
+    return render(request, 'pages/Laboratorios/laboratorios.html')
+
+def agendamentos(request):
+    return render(request, 'pages/Agendamentos/agendamentos.html')
+
 def novo_agendamento(request):
-    return render(request, 'pages/novo_agendamento.html')
+    return render(request, 'pages/Agendamentos/novo_agendamento.html')
+
